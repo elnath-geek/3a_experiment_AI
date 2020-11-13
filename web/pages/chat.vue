4ff41c0b675895e8d9d27cbb4a0108e48cbf143d
wolfram1293 <yuki1293@g.ecc.u-tokyo.ac.jp>
2020-11-13 23:33:36 +0900

    develop

---
+++ web/pages/chat.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    {{ message }}
+  </div>
+</template>
+
+<script>
+export default {
+  async asyncData(context) {
+    const response = await context.$axios.get('http://localhost:3000/api')
+    return {
+      message: response.data.message
+    }
+  }
+}
+</script>
