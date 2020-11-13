2cf50e607f2a1833007a3b3f6bf599a5cf509584
kosuke <k-yamamoto93@g.ecc.u-tokyo.ac.jp>
2020-11-13 12:35:42 +0900

    サーバーで文字コードがバグる…

--- web_server/app/app.js
+++ web_server/app/app.js
@@ -1,26 +1,30 @@
-var express    = require('express');
-var app        = express();
-var bodyParser = require('body-parser');
+const express    = require('express');
+const cors = require('cors')
+const app        = express();
+const bodyParser = require('body-parser');
+const {PythonShell} = require('python-shell');
 
 //body-parserの設定
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(cors())
 
-var port = process.env.PORT || 3001; // port番号を指定
+const port = process.env.PORT || 3001; // port番号を指定
 
+// GET http://localhost:3000/api/main
+app.post('/api/main',function(req,res){
+  // console.log(req.body.text)
+  // console.log(unescape(req.body.text))
+  const req_text = unescape(req.body.text)
 
-// GET http://localhost:3000/api/v1/
-app.get('/api/',function(req,res){
-
-  const pyshell = new PythonShell('@/src/text2emotion.py');
-    pyshell.send(this.text);
-    pyshell.on('ret', (err)=>{
-      console.log(ret);
-    })
-
-  res.json({
-      message:"Hello,world"
-  });
+  const pyshell = new PythonShell('./src/text2emotion.py');
+  pyshell.send(req_text);
+  pyshell.on('message', (ret)=>{
+    console.log(ret);
+    res.json({
+      message: ret
+    });
+  })
 });
 
 //サーバ起動
