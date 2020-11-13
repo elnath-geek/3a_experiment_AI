da4f87fa9a3ab76db32a41d2e10dbd096da8a900
kosuke <k-yamamoto93@g.ecc.u-tokyo.ac.jp>
2020-11-13 23:50:38 +0900

    google 翻訳の仕方がわからんち

--- web_server/app/app.js
+++ web_server/app/app.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser');
 const {PythonShell} = require('python-shell');
 const { text } = require('body-parser');
 const axios = require('axios')
+const qs = require("qs");
 
 //body-parserの設定
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -14,15 +15,19 @@ app.use(cors())
 const port = process.env.PORT || 3001; // port番号を指定
 
 function translate(text) {
-  url = 'https://script.google.com/macros/s/AKfycbwjPPfA-Br7ykp0wkujue6_STL3k9U8nYt1n_2XXOHXR_FpBmwo/exec'
-  payload = {"text": text, "source":"ja", "target": "en"}
-  axios.get(url, {
-    params: payload
-  }).then(res => {
+  const url = 'https://script.google.com/macros/s/AKfycbwjPPfA-Br7ykp0wkujue6_STL3k9U8nYt1n_2XXOHXR_FpBmwo/exec'
+  const params = qs.stringify({'text': text, 'source': 'ja', 'target': 'en'})
+  const headers = {
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded'
+    }
+  }
+
+  axios.get(url, params, headers).then(res => {
     console.log(res.data)
     rlist = res.data.split(/.*?[.!?]/)
     return rlist
-  }).cathch( err => {
+  }).catch( err => {
     console.log(err)
   })
 }
@@ -35,6 +40,7 @@ app.post('/api/main',function(req,res){
   const ja_split_sentence = ja_sentence.split(/[。！？!?]/)
   console.log(ja_split_sentence)
   const en_sentence = translate(ja_sentence)
+  console.log(en_sentence)
 
   // const pyshell = new PythonShell('./src/text2emotion.py');
   // pyshell.send(en_sentence);
