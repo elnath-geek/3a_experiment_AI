fdd992bd3f1f84f2d9157353722b05328a505a06
wolfram1293 <yuki1293@g.ecc.u-tokyo.ac.jp>
2020-11-17 16:51:22 +0900

    bug fixed

--- web_server/src/text2emotion.py
+++ web_server/src/text2emotion.py
@@ -25,7 +25,10 @@ def suggest(emotions, k=3):
 def main():
     device = torch.device("cpu")
     en_sentence = sys.stdin.readline()
-    en_split_sentence = np.array(re.findall(".*?[.!?]", en_sentence))
+    en_split_sentence = re.findall(".*?[.!?]", en_sentence)
+    if len(en_split_sentence) == 0:
+      en_split_sentence = [en_sentence]
+    en_split_sentence = np.array(en_split_sentence)
     # print("python-shell", type(en_split_sentence))
     # print("python-shell", en_split_sentence)
 
